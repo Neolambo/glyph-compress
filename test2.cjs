f7579be030aa572aaf5764d983ab7b8ce08a3406
dede7 <dede7@users.noreply.github.com>
2026-06-03 21:55:30 +0200

    chore: remove temporary scratchpad development files

--- test2.cjs
+++
@@ -1,4 +0,0 @@
-const { GlyphCompressor } = require('./vscode-ext/glyph-middleware.cjs');
-const gc = new GlyphCompressor({ level: 'aggressive' });
-const code = "```javascript\nfunction test() { const a = 1; let b = 2; return a + b; }\n```";
-console.log(gc.compressText(code).compressed);
